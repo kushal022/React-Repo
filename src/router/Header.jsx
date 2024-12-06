3c2068d5cdb28f8688c948c7c9684bd6377c0283
kushal jangid <kushal.learn22@gmial.com>
2024-12-06 23:39:58 +0530

    Router:- NavLink,Link,and Anchor tag

--- src/router/Header.jsx
+++ src/router/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
@@ -13,22 +13,26 @@ const Header = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Ancor tag: Home</a>
-                                <Link to='/' className="nav-link active" aria-current="page">Link tag: Home</Link>
+                                <a className="nav-link " aria-current="page" href="/">Ancor tag: Home</a>
+                                <Link to='/' className="nav-link " aria-current="page">Link tag: Home</Link>
+                                <NavLink to='/' className="nav-link " aria-current="page">NavLink tag: Home</NavLink>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="/contact">Ancor tag: Contact</a>
-                                <Link to='/contact' className="nav-link active" aria-current="page">Link tag: Contact</Link>
+                                <Link to='/contact' className="nav-link " aria-current="page">Link tag: Contact</Link>
+                                <NavLink to='/contact' className="nav-link " aria-current="page">NavLink tag: Contact</NavLink>
 
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="/about">Ancor tag: About</a>
-                                <Link to='/about' className="nav-link active" aria-current="page">Link tag: About</Link>
+                                <Link to='/about' className="nav-link " aria-current="page">Link tag: About</Link>
+                                <NavLink to='/about' className="nav-link " aria-current="page">NavLink tag: About</NavLink>
 
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link" href="/info">Ancor tag: Info</a>
-                                <Link to='/info' className="nav-link active" aria-current="page">Link tag: Info</Link>
+                                <Link to='/info' className="nav-link " aria-current="page">Link tag: Info</Link>
+                                <NavLink to='/info' className="nav-link " aria-current="page">NavLink tag: Info</NavLink>
 
                             </li>
                         </ul>
