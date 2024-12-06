53c529ac4ba9f24e557aaa5da5249a03f889353f
kushal jangid <kushal.learn22@gmial.com>
2024-12-06 18:10:00 +0530

    Router:create and link Error page

---
+++ src/router/ErrorPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ErrorPage = () => {
+    return (
+        <div style={{
+            textAlign: 'center'
+        }}>
+            <img
+                style={{
+                    width: '50%',
+                    height: '70vh',
+                    // objectFit: 'cover',
+                }}
+                src="https://th.bing.com/th/id/OIP.QbURL1GMcWBThvk4AP87kQAAAA?rs=1&pid=ImgDetMain" alt="" />
+
+        </div>
+    )
+}
+
+export default ErrorPage
--- src/router/RouterDemo2.jsx
+++ src/router/RouterDemo2.jsx
@@ -5,6 +5,7 @@ import Home from './Home';
 import Contact from './Contact';
 import Info from './Info';
 import About from './About';
+import ErrorPage from './ErrorPage';
 function RouterDemo2() {
     return (
         <Router>
@@ -14,6 +15,8 @@ function RouterDemo2() {
                 <Route path='/about' element={<About />} />
                 <Route path='/info' element={<Info />} />
                 <Route path='/contact' element={<Contact />} />
+                {/* Error page route if invalid path enter user */}
+                <Route path='/*' element={<ErrorPage />} />
             </Routes>
         </Router>
     )
